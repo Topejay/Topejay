86bb030692f698bfc8d3877422e33e5df25f141f
Topejay <121813650+Topejay@users.noreply.github.com>
2023-02-21 11:09:13 +0100

    REACT
    Here is my first react Code. I'm happy☺

---
+++ index.js
@@ -0,0 +1,7 @@
+ReactDOM.render(<h1 align = "center">  Hello Reactor</h1>, document.getElementById("root"))
+
+
+ReactDOM.render(<h2 align = "center" ><button> <font size ="9">HERE IS A REACT PAGE, COMPLETE REACT AND FACE DATA ANALYTICS</font></button></h2>, document.getElementById("root"))
+
+
+
